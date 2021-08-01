c0c17aa6136ee27cd7d21f56436ee0f254e2c6ef
bexking <bex.king@icloud.com>
2021-08-02 11:28:05 +1200

    cleaning up code in contact footer

--- src/Components/JS/contact-footer.js
+++ src/Components/JS/contact-footer.js
@@ -1,7 +1,5 @@
 import React from 'react';  
-import { Link } from "react-router-dom";
 import '../CSS/contact-footer.css';
-import Me from '../../Assets/self.png';
 import Contact from '../../Assets/contact.png';
 import Insta from '../../Assets/insta.png';
 
