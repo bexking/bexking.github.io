178af8a9ca521644750ad5385f76511d2ec8b5af
bexking <bex.king@icloud.com>
2021-08-02 11:29:31 +1200

    cleaning up imports on work page

--- src/Pages/JS/work.js
+++ src/Pages/JS/work.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import ProjectGrid from '../../Components/JS/projectgrid.js'
-import Nav from '../../Components/JS/nav2.js'
-import ContactFooter from '../../Components/JS/contact-footer'
+import ProjectGrid from '../../Components/JS/projectgrid.js';
 
 export default function Work() {
     return (
@@ -9,7 +7,6 @@ export default function Work() {
            <div id="workworkwork">
                 <h2 className="homeh1" id="work">My Work</h2>
             </div>
-
             <ProjectGrid />
         </div>
     )
