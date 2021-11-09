218edf9c86db16d02726f82e2848cf58c8e14521
bexking <bex.king@icloud.com>
2021-11-09 13:06:50 +1300

    updating grid order

--- src/Components/JS/projectgrid.js
+++ src/Components/JS/projectgrid.js
@@ -11,9 +11,9 @@ export default function ProjectGrid() {
 
             <div className="projectGrid">
                 <WorkNavItem project={projects.well} className="hero-image"/>
-                <WorkNavItem project={projects.rm} className="hero-image"/>
                 <WorkNavItem project={projects.hints} className="hero-image"/>
                 <WorkNavItem project={projects.ib} className="hero-image"/>
+                <WorkNavItem project={projects.rm} className="hero-image"/>
                 <WorkNavItem project={projects.displace} className="hero-image"/>
                 <WorkNavItem project={projects.acDf} className="hero-image"/>
                 {/* <WorkNavItem project={projects.cn} className="hero-image"/> */}
