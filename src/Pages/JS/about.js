c1b73e207f271b2cfbf7c496f0b13cb1e482c5ed
Bex <bexkingetc@gmail.com>
2024-01-22 23:42:12 +1300

    adding extra section of info to about page

--- src/Pages/JS/about.js
+++ src/Pages/JS/about.js
@@ -29,6 +29,37 @@ export default function About() {
                     </p>             
                     <p className="homepage-p">Check out my <Link to="#workworkwork" className="homepage-links"  id="homepage-link1">work</Link> and get in touch at <a href="mailto:bexkingdesign@gmail.com"  className="homepage-links" id="homepage-link2">bexkingdesign@gmail.com</a>. </p>
                     <p className="homepage-p">Ngā mihi nui</p>
+                    <div className="info-bullets">
+                        <div className="info-bullet-group">
+                            <h5 className='xp'>Skills</h5>
+                            <hr></hr>
+                            <p className='xp-para'>User experience design</p>
+                            <p className='xp-para'>User interface design</p>
+                            <p className='xp-para'>Human-centered design</p>
+                            <p className='xp-para'>User research & testing</p>
+                            <p className='xp-para'>Qualitative research</p>
+                            <p className='xp-para'>Visual design</p>
+                        </div>
+                        <div className="info-bullet-group">
+                            <h5 className='xp'>Education</h5>
+                            <hr></hr>
+                            <div className="edu-para">
+                                <p className='xp-para-edu'>Bachelor of Design Innovation (Interaction Design)</p>
+                                <p className='xp-para-edu'>Victoria University of Wellington</p>
+                                <p className='xp-para-edu'>2018-2020</p>
+                            </div>
+                            <div>
+                                <p className='xp-para-edu'>Certificate in Māori Protocol and Language (Level 4)</p>
+                                <p className='xp-para-edu'>Te Wānanga o Raukawa</p>
+                                <p className='xp-para-edu'>2021</p>
+                            </div>
+                        </div>
+                        <div className="info-bullet-group">
+                            <h5 className='xp'>Resume</h5>
+                            <hr></hr>
+                            <p className='xp-para'>Available on request</p>
+                        </div>
+                    </div>
                 </div>
             </div>    
         </div>
