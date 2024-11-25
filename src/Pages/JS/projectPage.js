b6e14c53e4bdda7f71e1becaf7f08cd5a49f56ec
Bex <bexkingetc@gmail.com>
2024-11-25 22:58:43 +1300

    Making project template responsive

--- src/Pages/JS/projectPage.js
+++ src/Pages/JS/projectPage.js
@@ -11,15 +11,14 @@ export default function ProjectPage() {
                     <div className="text-block-third">
                         <span className='upper-title'>Pre-title</span>
                         <h1 className="projectTitle">Project Title</h1>
-                        <p>Here is a bit of a blurb which I will try and make roughly the length of a project blurb. When making this blurb i typed a lot on the computer, as I just wanted to get an idea of the spacing. Once I had done that I reviewed it. In the next phase of work I will test and refine the CSS to see what is/isn't working.</p>
                         <p className='type-of-work'>Type of work</p>
+                        <p>Here is a bit of a blurb which I will try and make roughly the length of a project blurb. When making this blurb i typed a lot on the computer, as I just wanted to get an idea of the spacing. Once I had done that I reviewed it. In the next phase of work I will test and refine the CSS to see what is/isn't working.</p>
                         <a href="" className='external-link'>Link to project</a>
                     </div>
-                    <div className="project-two-thirds">
-                        <img id="test-image" src={Dis1} alt="Four small weatherboard huts sit in an icy coastal scene" />
+                    <div className="project-two-thirds case-study-hero">
+                        <img id="test-image1" src={Dis1} alt="Four small weatherboard huts sit in an icy coastal scene" />
                     </div>
                 </div> 
-
                 <div className='full-width-content-block'>
                     <img id="full-width-test" src={Dis1} alt="Four small weatherboard huts sit in an icy coastal scene" />
                 </div>  
