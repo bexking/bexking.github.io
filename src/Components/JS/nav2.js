0543fe1b15fb69c35468ac96004789401d3f9ec0
Bex <bexkingetc@gmail.com>
2024-11-26 22:42:39 +1300

    Styled form, added hamburger

--- src/Components/JS/nav2.js
+++ src/Components/JS/nav2.js
@@ -1,27 +1,38 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from "react-router-dom";
 import '../CSS/nav.css';
 import Logo from '../../Assets/home13.svg'
 
 export default function Nav() {
+    const [showBurger, setBurger] = useState(false);
+
+    const links = <>
+        {/* <Link to="/work" id="nav-places" className='large-link'>Work</Link> */}
+        <Link to="/about" id="nav-places" className='large-link'>About</Link>
+        <Link to="/contact" id="nav-places" className='large-link'>Contact</Link>
+    </>;
+
     return (
         <nav id="main-nav">
             <div id="nav-contents">
                 <Link to="/home"><img className="nav-logo" src={Logo} alt="logo saying Bex King"/></Link>
                 <div className="nav-pages"> 
-                    {/* <Link to="/work" id="nav-places" className='large-link'>Work</Link> */}
-                    <Link to="/about" id="nav-places" className='large-link'>About</Link>
-                    <Link to="/contact" id="nav-places" className='large-link'>Contact</Link>
+                    {links}
                 </div>
             </div>
 
             <div id='hamburger-nav'>
-                <svg id="hamburger" viewBox="0 0 100 80" width="30" height="40">
+                <svg onClick={() => setBurger(!showBurger)} id="hamburger" viewBox="0 0 100 80" width="30" height="40">
                     <rect width="100" height="10" rx="8"></rect>
                     <rect y="30" width="100" height="10" rx="8"></rect>
                     <rect y="60" width="100" height="10" rx="8"></rect>
                 </svg>
             </div>
+
+            {showBurger && <div>
+                {links}
+            </div>}
+
         </nav>
 
         
--- src/Pages/JS/contact.js
+++ src/Pages/JS/contact.js
@@ -10,28 +10,28 @@ export default function Contact() {
                     <div className='bio-container'>
                         <h1 id="bio-h1">Whakapā mai</h1>
                         <p className='intro-large'>
-                            Kia ora, drop me an email down below! </p>
+                            Kia ora. Thanks for visiting my portfolio. If you'd like to get in touch, flick me an email using the form. </p>
                     </div>
-                </div>
-                <form action="https://usebasin.com/f/d65363db7fb5" method="post"> {/* Keep this line the same, change other form elements as necessary */}
-                    <div className='contact-form'>
-                        <div className='fields'>
-                            <label>Your email address
-                                <input name="email" type="text" placeholder="Your email..."/>
-                            </label>
-                            <label>Subject
-                                <input name="subject" type="text" placeholder="Subject..."/>
-                            </label>
-                            <label>Message
-                                <textarea name="message" type="text" class="form-section" cols="45" rows="8" placeholder='Your message...'></textarea>
-                            </label>
-                                {/* <input type="submit"/> */}
-                                <button type="submit" className='button-reg'>Send</button>
-
+                
+                    <form action="https://usebasin.com/f/d65363db7fb5" method="post"> {/* Keep this line the same, change other form elements as necessary */}
+                        <div className='contact-form'>
+                            <div className='fields'>
+                            <h2 className='contact form-box'>Contact form</h2>
+                                <label>Your email address:
+                                    <input name="email" type="text" placeholder="Your email..."/>
+                                </label>
+                                <label>Subject:
+                                    <input name="subject" type="text" placeholder="Subject..."/>
+                                </label>
+                                <label>Message:
+                                    <textarea name="message" type="text" class="form-section" cols="45" rows="8" placeholder='Your message...'></textarea>
+                                </label>
+                                    {/* <input type="submit"/> */}
+                                    <button type="submit" className='button-wide'>Send</button>
+                            </div>
                         </div>
-                        
-                    </div>
-                </form>
+                    </form>  
+                </div>
             </div>
     </div>
     )
