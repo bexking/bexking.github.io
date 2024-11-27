a93ffa8542bf36151e0eea94bfd60e413667b472
Bex <bexkingetc@gmail.com>
2024-11-28 01:07:55 +1300

    styling hamburger menu and adding onclick to ensure hamburger closes after page navigation

--- src/Components/JS/nav2.js
+++ src/Components/JS/nav2.js
@@ -8,8 +8,8 @@ export default function Nav() {
 
     const links = <>
         {/* <Link to="/work" id="nav-places" className='large-link'>Work</Link> */}
-        <Link to="/about" id="nav-places" className='large-link'>About</Link>
-        <Link to="/contact" id="nav-places" className='large-link'>Contact</Link>
+        <Link onClick={() => setBurger(!showBurger)} to="/about" id="nav-places" className='large-link'>About</Link>
+        <Link onClick={() => setBurger(!showBurger)} to="/contact" id="nav-places" className='large-link'>Contact</Link>
     </>;
 
     return (
@@ -22,15 +22,21 @@ export default function Nav() {
             </div>
 
             <div id='hamburger-nav'>
+                <Link to="/home"><img className="nav-logo" src={Logo} alt="logo saying Bex King"/></Link>
                 <svg onClick={() => setBurger(!showBurger)} id="hamburger" viewBox="0 0 100 80" width="30" height="40">
                     <rect width="100" height="10" rx="8"></rect>
-                    <rect y="30" width="100" height="10" rx="8"></rect>
+                    <rect y="30" width="100" height="10" rx="8" id="mid-bar"></rect>
                     <rect y="60" width="100" height="10" rx="8"></rect>
                 </svg>
             </div>
 
-            {showBurger && <div>
+            {showBurger && <div id="hamburger-list">
                 {links}
+                <div id="home-wave">
+                    <svg id="wave1" data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                        <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" className="shape-fill"></path>
+                    </svg>
+                </div>
             </div>}
 
         </nav>
--- src/Pages/JS/contact.js
+++ src/Pages/JS/contact.js
@@ -1,8 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useHistory } from "react-router-dom";
 import { HashLink as Link } from 'react-router-hash-link';
 import '../CSS/contact.css';
 
 export default function Contact() {
+    const history = useHistory();
+    const [formData, setFormData] = useState({
+        email: "",
+        subject: "",
+        message: ""
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+          ...formData,
+          [name]: value,
+        });
+    };
+
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+    
+        await fetch("https://usebasin.com/f/d65363db7fb5", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(formData),
+            }).then((value) => {
+                history.push("/home"); // Redirect after form is finished submitting. You can change this
+            })
+      };
+      
     return (
         <div>
            <div className="work-feature">
@@ -13,18 +44,18 @@ export default function Contact() {
                             Kia ora. Thanks for visiting my portfolio. If you'd like to get in touch, flick me an email using the form. </p>
                     </div>
                 
-                    <form action="https://usebasin.com/f/d65363db7fb5" method="post"> {/* Keep this line the same, change other form elements as necessary */}
+                    <form onSubmit={handleSubmit}>
                         <div className='contact-form'>
                             <div className='fields'>
                             <h2 className='contact form-box'>Contact form</h2>
                                 <label>Your email address:
-                                    <input name="email" type="text" placeholder="Your email..."/>
+                                    <input name="email" type="text" placeholder="Your email..." value={formData.email} onChange={handleChange} />
                                 </label>
                                 <label>Subject:
-                                    <input name="subject" type="text" placeholder="Subject..."/>
+                                    <input name="subject" type="text" placeholder="Subject..." value={formData.subject} onChange={handleChange} />
                                 </label>
                                 <label>Message:
-                                    <textarea name="message" type="text" class="form-section" cols="45" rows="8" placeholder='Your message...'></textarea>
+                                    <textarea name="message" type="text" class="form-section" cols="45" rows="8" placeholder='Your message...' value={formData.message} onChange={handleChange}></textarea>
                                 </label>
                                     {/* <input type="submit"/> */}
                                     <button type="submit" className='button-wide'>Send</button>
