57c60bb85a7a6172e30cae9bf594c6d5530e2b16
Bex King <bexkingdesign@gmail.com>
2024-11-28 23:17:25 +1300

    adjusting project order

--- src/Components/JS/projectgrid.js
+++ src/Components/JS/projectgrid.js
@@ -10,12 +10,11 @@ export default function ProjectGrid() {
             <div className="projectGrid">
                 <WorkNavItem project={projects.business} className="hero-image"/>
                 <WorkNavItem project={projects.ib} className="hero-image"/>
-                <WorkNavItem project={projects.rm} className="hero-image"/>
                 <WorkNavItem project={projects.acDf} className="hero-image"/>
+                <WorkNavItem project={projects.rm} className="hero-image"/>
                 <WorkNavItem project={projects.hints} className="hero-image"/>
-                <WorkNavItem project={projects.template} className="hero-image"/>
-                <WorkNavItem project={projects.well} className="hero-image"/>
-                
+                {/* <WorkNavItem project={projects.template} className="hero-image"/> */}
+                {/* <WorkNavItem project={projects.well} className="hero-image"/> */}
                 {/* <WorkNavItem project={projects.displace} className="hero-image"/> */}
                 {/* <WorkNavItem project={projects.cn} className="hero-image"/> */}
             </div>
