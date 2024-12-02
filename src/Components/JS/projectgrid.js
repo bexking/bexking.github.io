f5a69a81f4129d7dbf165cb4d812337049ed39a8
Bex <bexkingetc@gmail.com>
2024-12-03 02:28:37 +1300

    completing edits to hints content

--- src/Components/JS/projectgrid.js
+++ src/Components/JS/projectgrid.js
@@ -9,10 +9,10 @@ export default function ProjectGrid() {
     return (
             <div className="projectGrid">
                 <WorkNavItem project={projects.business} className="hero-image"/>
-                <WorkNavItem project={projects.ib} className="hero-image"/>
-                <WorkNavItem project={projects.acDf} className="hero-image"/>
+                {/* <WorkNavItem project={projects.ib} className="hero-image"/> */}
                 {/* <WorkNavItem project={projects.rm} className="hero-image"/> */}
                 <WorkNavItem project={projects.hints} className="hero-image"/>
+                <WorkNavItem project={projects.acDf} className="hero-image"/>
                 {/* <WorkNavItem project={projects.template} className="hero-image"/> */}
                 {/* <WorkNavItem project={projects.well} className="hero-image"/> */}
                 {/* <WorkNavItem project={projects.displace} className="hero-image"/> */}
