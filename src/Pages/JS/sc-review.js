fe02b73f63ee78458529380a65ca5af0ac51069e
Bex King <bex.king@icloud.com>
2025-01-16 20:11:24 +1300

    adding text to sensitive claims page

--- src/Pages/JS/sc-review.js
+++ src/Pages/JS/sc-review.js
@@ -23,14 +23,18 @@ export default function ScReview() {
 
                 <div className='body-copy'>
                     <p>
-                        
+                        Following the Royal Commission of Inquiry into Abuse in Care in Aotearoa, a public-sector client wanted us to review and improve the sensitive claims section of their website. They wanted to improve the experience people have when deciding to make a claim, as well as during the claims process. They sought to improve transparency and accessibility for those using the site, and to build trust.
                     </p>
                     <p>
-                       
+                       I worked with two colleagues to review the existing site, conduct research, and create digital prototypes for desired changes. 
+                       The research phase included hosting workshops with client subject matter experts (SMEs) to learn about the existing process and pain points, and interviewing legal experts and accessibility experts who had experience supporting people through this sensitive claims process. 
                     </p>
                     <p>
-                        
+                        Our findings were presented back to the client SMEs and became guiding principles to co-design and prototype proposed changes. I led a small group focusing on "Process Visibility" - a section focused on clearly showing what happens at each phase of the process Many who had worked with claimants shared that this information was not easy to access, and that the lack of transparency made it hard for people lodging claims to know what to expect.
                     </p>  
+                    <p>
+                        As a team, my colleages and I created medium fidelity prototypes of the Process Visibility section, an eligibility tool to help people deciding to make a claim understand if they are in the right place, and a more user-friendly digital submission form. Internal testing and feedback was conducted in a workshop and the final proposed designs were provided to the client for any further testing and implementation required.
+                    </p>
                 </div>
                 {/* <div className='full-width-content-block'>
                     <img className='full-width-test' src={Exec} alt="A4 booklet open to the General Themes section of the report"/>
